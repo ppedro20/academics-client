92ff59872f60b7e67713dfd57ee47f18f3e8d5e2
Pedro Pedro <pedro.afonso.pedro.pp@gmail.com>
2023-10-12 12:49:11 +0100

    ficha 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      API_URL: process.env.API_URL || 'http://localhost:8080/academics/api'
+    }
+  }
 })
